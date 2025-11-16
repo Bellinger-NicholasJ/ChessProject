0db945f3540787808d02afc39de15afc4afa406f
Bellinger-Nicholas-J <bellinger.nicholasj@gmail.com>
2025-11-16 14:10:01 -0800

    Project setup complete. Setup file structure, project and license documentation, a hello world test, a cmake build system, and precompiled headers.

---
+++ build/CMakeFiles/chess.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/the-architekt/Dev/ChessProject/build/CMakeFiles/chess.dir/cmake_pch.hxx.cxx" "CMakeFiles/chess.dir/cmake_pch.hxx.pch" "gcc" "CMakeFiles/chess.dir/cmake_pch.hxx.pch.d"
+  "/Users/the-architekt/Dev/ChessProject/build/CMakeFiles/chess.dir/cmake_pch.hxx" "CMakeFiles/chess.dir/cmake_pch.hxx.pch" "gcc" "CMakeFiles/chess.dir/cmake_pch.hxx.pch.d"
+  "/Users/the-architekt/Dev/ChessProject/src/main.cpp" "CMakeFiles/chess.dir/src/main.cpp.o" "gcc" "CMakeFiles/chess.dir/src/main.cpp.o.d"
+  "/Users/the-architekt/Dev/ChessProject/build/CMakeFiles/chess.dir/cmake_pch.hxx" "CMakeFiles/chess.dir/src/main.cpp.o" "gcc" "CMakeFiles/chess.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
